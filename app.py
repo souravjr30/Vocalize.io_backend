ee9c3b534c21d5a03cdf3b214b5b096eb055a7b2
Sourav J Raju <souravjraju333@gmail.com>
2025-01-29 16:59:12 +0530

    Changed port number

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def download_file():
     app.run(debug=True)'''
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
 
